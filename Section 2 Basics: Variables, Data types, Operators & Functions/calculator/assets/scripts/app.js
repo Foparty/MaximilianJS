b6486f035e5cfbb770c1f1a57aaa805b5107ac45
Foparty <103907142+Foparty@users.noreply.github.com>
2022-05-18 12:20:21 +0200

    until the internet worked

--- Section 2 Basics: Variables, Data types, Operators & Functions/calculator/assets/scripts/app.js
+++ Section 2 Basics: Variables, Data types, Operators & Functions/calculator/assets/scripts/app.js
@@ -1,10 +1,20 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+
+function add(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
+
+currentResult = add (1, 2);
+// console.log(currentResult);
+
+
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 // console.log(currentResult);
 
 let calculationDescription = `( ${currentResult}  + 10) * 3 / 2 - 1`;
-let errorMessage = 'An error ocurred';
+// let errorMessage = 'An error ocurred';
 outputResult(currentResult, calculationDescription);
