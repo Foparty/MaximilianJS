1558e1ea838425d90f4f8616acb11bebe6ff6211
Foparty <103907142+Foparty@users.noreply.github.com>
2022-05-18 11:00:19 +0200

    moved folders

---
+++ Section 2 Basics: Variables, Data types, Operators & Functions/assignment/assignment.js
@@ -0,0 +1,12 @@
+const inputValue = 14;
+let result = '';
+
+result = inputValue + 18;
+console.log(result);
+result = result * 3;
+console.log(result);
+result = result * 3;
+console.log(result);
+alert(`The initial value is ${inputValue}`);
+alert(`The final result is ${result}`);
+
