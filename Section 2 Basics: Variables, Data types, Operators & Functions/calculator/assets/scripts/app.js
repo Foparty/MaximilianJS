45269948f18baf9c5e677966c2e239f4c239f5ff
Foparty <103907142+Foparty@users.noreply.github.com>
2022-05-19 14:02:29 +0200

    jsjsjsjs

--- Section 2 Basics: Variables, Data types, Operators & Functions/calculator/assets/scripts/app.js
+++ Section 2 Basics: Variables, Data types, Operators & Functions/calculator/assets/scripts/app.js
@@ -1,10 +1,12 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// gets input from inout field
 function getUserInput () {
   return parseInt(usrInput.value)
 }
 
+// generates and writes calculation log
 function createAndWriteOutput (operator, resultBeforeCalc, calcNumber) {
   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
   outputResult(currentResult, calcDescription);
@@ -14,7 +16,7 @@ function add() {
   const enteredNumber = getUserInput();
   const calcDescription = `${currentResult} + ${enteredNumber}`;
   const initialResult = currentResult;
-  currentResult = currentResult + enteredNumber;
+  currentResult += enteredNumber;
   createAndWriteOutput('+', initialResult, enteredNumber);
 }
 
@@ -22,21 +24,21 @@ function substract () {
   const enteredNumber = getUserInput();
   const calcDescription = `${currentResult} - ${enteredNumber}`;
   const initialResult = currentResult;
-  currentResult = currentResult + enteredNumber;
+  currentResult -= enteredNumber;
   createAndWriteOutput('-', initialResult, enteredNumber);
 }
 function multiply () {
   const enteredNumber = getUserInput();
   const calcDescription = `${currentResult} * ${enteredNumber}`;
   const initialResult = currentResult;
-  currentResult = currentResult * enteredNumber;
+  currentResult *= enteredNumber;
   createAndWriteOutput('*', initialResult, enteredNumber);
 }
 function divide () {
   const enteredNumber = getUserInput();
   const calcDescription = `${currentResult} / ${enteredNumber}`;
   const initialResult = currentResult;
-  currentResult = currentResult / enteredNumber;
+  currentResult /= enteredNumber;
   createAndWriteOutput('/', initialResult, enteredNumber);
 }
 
